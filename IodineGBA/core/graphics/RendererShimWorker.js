2353151818dafe56fbce78a51baaecc6e7ffc836
Grant Galitz <grantgalitz@gmail.com>
2016-01-23 04:30:02 -0500

    need to make the other side of the thread

---
+++ IodineGBA/core/graphics/RendererShimWorker.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ Copyright (C) 2012-2016 Grant Galitz
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+importScripts("../IodineGBA/core/graphics/Renderer.js");
+importScripts("../IodineGBA/core/graphics/RendererProxy.js");
+importScripts("../IodineGBA/core/graphics/BGTEXT.js");
+importScripts("../IodineGBA/core/graphics/BG2FrameBuffer.js");
+importScripts("../IodineGBA/core/graphics/BGMatrix.js");
+importScripts("../IodineGBA/core/graphics/AffineBG.js");
+importScripts("../IodineGBA/core/graphics/ColorEffects.js");
+importScripts("../IodineGBA/core/graphics/Mosaic.js");
+importScripts("../IodineGBA/core/graphics/OBJ.js");
+importScripts("../IodineGBA/core/graphics/OBJWindow.js");
+importScripts("../IodineGBA/core/graphics/Window.js");
+importScripts("../IodineGBA/core/graphics/Compositor.js");
