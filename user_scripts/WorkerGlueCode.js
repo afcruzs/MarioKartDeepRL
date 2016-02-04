e2c4adcae152b86744fba056c11e78acd085eac2
Grant Galitz <gegalitz3491@eagle.fgcu.edu>
2016-02-04 13:24:50 -0500

    Forward some error capture from webworker

--- user_scripts/WorkerGlueCode.js
+++ user_scripts/WorkerGlueCode.js
@@ -9,6 +9,7 @@
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 function IodineGBAWorkerShim() {
+	this.playError = null;
     this.gfx = null;
     this.audio = null;
     this.speed = null;
@@ -65,6 +66,14 @@ IodineGBAWorkerShim.prototype.timerCallback = function (timestamp) {
         //Forward latest timestamp to worker:
         Atomics.store(this.timestamp, 0, timestamp >>> 0);
     }
+}
+IodineGBAWorkerShim.prototype.attachPlayErrorHandler = function (playError) {
+	this.playError = playError;
+}
+IodineGBAWorkerShim.prototype.issuePlayError = function () {
+	if (this.playError) {
+		this.playError();
+	}
 }
 IodineGBAWorkerShim.prototype.attachGraphicsFrameHandler = function (gfx) {
     this.gfx = gfx;
@@ -159,8 +168,11 @@ IodineGBAWorkerShim.prototype.decodeMessage = function (data) {
         case 7:
             this.saveExportRequest(data.saveID, data.saveData);
             break;
-        default:
+        case 8:
             this.speedPush(+data.speed);
+			break;
+		default:
+			this.issuePlayError();
     }
 }
 IodineGBAWorkerShim.prototype.audioInitialize = function (channels, sampleRate, bufferLimit) {
