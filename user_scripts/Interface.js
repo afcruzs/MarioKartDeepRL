0ca17cd800647438f6bcb5703e2872c54aebcb9b
Jeff Zhang <jeffzhang@Jeffs-MacBook-Pro.local>
2016-07-04 02:20:46 -0700

    add voice interface file

--- user_scripts/Interface.js
+++ user_scripts/Interface.js
@@ -1,9 +1,9 @@
-var mapButtontoIndex = {"A":0, "B":1, "Select":2, "Start":3, "Right":4, "Left": 5, "Up":6 ,"Down":7, "R":8, "L":9}
+var mapButtontoIndex = {"a":0, "b":1, "select":2, "start":3, "right":4, "left": 5, "up":6 ,"down":7, "r":8, "l":9}
 
 function pressButton(button, time) {
 
 	if(button in mapButtontoIndex ){
-		
+
 		index = mapButtontoIndex[button]	
 		IodineGUI.Iodine.keyDown(index);
 
