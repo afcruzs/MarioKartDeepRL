5c7eb11d6b2a7ca4b992459dbc7eaa70094c727a
Daniel Castro <dancas2@gmail.com>
2016-11-13 02:22:10 -0500

    Reward function fixes

--- emulator-scripts/mario_kart.lua
+++ emulator-scripts/mario_kart.lua
@@ -14,6 +14,12 @@ local minimap_offset_y = 160 - 64
 local end_of_lap_threshold = 0.9
 local start_of_lap_threshold = 0.1
 
+local max_time_without_finish = 18000
+local max_frames = 65535
+local max_coins = 255
+local max_entity_hits = 20
+local max_wall_hits = 255
+
 function get_lap()
   local status = memory.read_u8(0x3BE0)
   if status == 4 then
@@ -33,16 +39,36 @@ function compute_reward(track_info)
   local first_lap_time = memory.read_u16_le(0x3C74)
   local second_lap_time = memory.read_u16_le(0x3C76)
   local current_lap_time = memory.read_u16_le(0x5C80) - first_lap_time - second_lap_time
+
   local frames_not_hitting_gas = memory.read_u16_le(0x3ADC)
+  -- Overflow check
+  if memory.read_u8(0x3ADE) == 1 then
+    frames_not_hitting_gas = max_frames
+  end
+
   local frames_hitting_brake = memory.read_u16_le(0x3AE0)
+  -- Overflow check
+  if memory.read_u8(0x3AE2) == 1 then
+    frames_hitting_brake = max_frames
+  end
+
   local lakitu_rescue_count = memory.read_u8(0x3AE7)
   local entity_hit_count = memory.read_u8(0x3AE8)
+  if entity_hit_count > max_entity_hits then
+    entity_hit_count = max_entity_hits
+  end
+
   local wall_hit_count = memory.read_u8(0x3AE9)
   local spin_count = memory.read_u8(0x3AEA)
   local start_turbo_count = memory.read_u8(0x3AEB)
   local drift_turbo_count = memory.read_u8(0x3AEC)
   local item_box_hit_full = memory.read_u8(0x3AF4)
+
   local frames_outside = memory.read_u16_le(0x3AF0)
+  -- Overflow check
+  if memory.read_u8(0x3AF2) == 1 then
+    frames_outside = max_frames
+  end
 
   local track_position = get_minimap_position()
   local x = track_position[1] - minimap_offset_x + 1
@@ -74,38 +100,39 @@ function compute_reward(track_info)
   local relative_time = average_time * current_lap_percentage - current_lap_time
   lap_reward[global_lap] = relative_time
 
+  --[[
   gui.text(0, 80, "Time reward lap 1: " .. lap_reward[1])
   gui.text(0, 100, "Time reward lap 2: " .. lap_reward[2])
   gui.text(0, 120, "Time reward lap 3: " .. lap_reward[3])
+  --]]
 
   -- return lap_reward[1] + lap_reward[2] + lap_reward[3]
   -- local relative_time = ((average_time - first_lap_time) + (average_time - second_lap_time) +
      -- average_time * lap_percentage - current_lap_time)
 
-  --[[
+  local time_reward = (lap_reward[1] + lap_reward[2] + lap_reward[3]) / (max_time_without_finish * 1.0)
+  local position_reward = (7 - position) / 7.0
+  local no_gas_reward = frames_not_hitting_gas / (max_frames * 1.0)
+  local brake_reward = frames_hitting_brake / (max_frames * 1.0)
+  local entity_hits_reward = entity_hit_count / (max_entity_hits * 1.0)
+  local wall_hits_reward = wall_hit_count / (max_wall_hits * 1.0)
+  local outside_reward = frames_outside / (max_frames * 1.0)
+
   local attributes = {
-    position=position, coins=coins, time=time,
-    no_hit_gas=frames_not_hitting_gas, brake_hits=frames_hitting_brake,
-    lakitu_rescues=lakitu_rescue_count, hits=entity_hit_count,
-    wall_hits=wall_hit_count, spins=spin_count, start_turbo=start_turbo_count,
-    drift_turbo=drift_turbo_count, item_box_full=item_box_hit_full,
-    frames_outside=frames_outside
+    time_reward=time_reward, position_reward=position_reward, no_gas_reward=no_gas_reward,
+    brake_reward=brake_reward, entity_hits_reward=entity_hits_reward,
+    wall_hits_reward=wall_hits_reward, outside_reward=outside_reward
   }
 
   local i = 0
   for key,value in pairs(attributes) do
-    gui.text(0, i * 20, key .. ": " .. value)
+    gui.text(0, i * 15 + 80, key .. ": " .. value)
     i = i + 1
   end
---]]
 
-  return (lap_reward[1] + lap_reward[2] + lap_reward[3]) / 80 +
-    (7 - position) * 8 + coins * 4 -
-    frames_not_hitting_gas / 4 - frames_hitting_brake * 2 -
-    lakitu_rescue_count * 30 - entity_hit_count * 15 -
-    wall_hit_count * 20 - spin_count * 15 + start_turbo_count * 25 +
-    drift_turbo_count * 15 + item_box_hit_full * 15 -
-    frames_outside / 4
+  return 0.4 * time_reward + 0.1 * position_reward - 0.1 * no_gas_reward - 0.075 * brake_reward -
+    0.025 * entity_hits_reward - 0.1 * wall_hits_reward - 0.2 * outside_reward
+
 end
 
 function get_minimap_position()
@@ -167,7 +194,6 @@ local frames_to_stack = 4
 local frame_number = 0
 local update_frequency = 10
 local action = {}
-local max_time_without_finish = 18000
 local train = true
 
 console.log(game_id)
@@ -204,7 +230,7 @@ while true do
   local out_of_time = (time >= max_time_without_finish)
 
   if out_of_time then
-    reward = -30000
+    reward = -1
   end
 
   if out_of_time or (frame_number % update_frequency) == 0 then
