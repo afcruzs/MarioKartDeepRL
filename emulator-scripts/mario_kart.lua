371e85f1d5346807dfecbb07044661003addd83b
Daniel Castro <dancas2@gmail.com>
2016-11-20 21:12:59 -0500

    Move state tracking and reward-related functions to MarioKartState class

--- emulator-scripts/mario_kart.lua
+++ emulator-scripts/mario_kart.lua
@@ -7,117 +7,129 @@ local deque = require("deque")
 local frames_to_stack = 4
 local frame_number = 0
 local update_frequency = 10
-local action = {}
-local train = true
-
-local current_pos = 0
-local prev_pos = 0
-local positions_queue = deque.new()
-
-local current_lap_percentage = 0
-local last_lap_percentage = 0.9
-local last_updated_lap = -1
-local lap_reward = {0, 0, 0}
-local actual_global_lap = 0
-local global_lap = 1
 local minimap_offset_x = 240 - 64
 local minimap_offset_y = 160 - 64
 local end_of_lap_threshold = 0.9
 local start_of_lap_threshold = 0.1
-
 local max_time_without_finish = 2000
 local max_frames = 65535
 local max_coins = 255
 local max_entity_hits = 20
 local max_wall_hits = 255
 local max_length_positions_queue = 60
+local lap_checkpoint_interval = 0.15
+local base_url = "http://localhost:5000/"
+local screenshot_folder = "../results/"
+local state_file = "../game/mario_kart.State"
 
-function push_position(position)
-    positions_queue:push_right(position)
-    if positions_queue:length() == max_length_positions_queue + 1 then
-        positions_queue:pop_left()
-    end
+local game_id = nil
+local train = true
+local action = {}
+local state = nil
+
+local MarioKartState = {}
+MarioKartState.__index = MarioKartState
+
+function MarioKartState.new()
+  local self = setmetatable({}, MarioKartState)
+
+  self:reset()
+  return self
 end
 
-function get_oldest_position()
-    return positions_queue:peek_left()
+function MarioKartState:reset()
+  self.current_lap_percentage = 0
+  self.last_lap_percentage = 0.9
+  self.actual_global_lap = 0
+  self.global_lap = 1
+  self.position = {0, 0}
+  self.time = 0
+  self.positions_queue = deque.new()
 end
 
-function get_lap()
-  local status = memory.read_u8(0x3BE0)
-  if status == 4 then
-    return 2
+function MarioKartState:_record_position(position)
+  self.positions_queue:push_right(position)
+  if self.positions_queue:length() == max_length_positions_queue + 1 then
+      self.positions_queue:pop_left()
   end
-
-  if status == 12 then
-    return 3
-  end
-
-  return 1
 end
 
-function compute_reward(track_info)
+function MarioKartState:_get_speed(track_info)
   local max_velocity = 100.0 * track_info['max_steps'] / track_info['average_time']
+  local new_position = assert(self.positions_queue:peek_right(), 'No new position available')
+  local old_position = assert(self.positions_queue:peek_left(), 'No old position available')
 
-  local track_position = get_minimap_position()
-  local x = track_position[1] - minimap_offset_x + 1
-  local y = track_position[2] - minimap_offset_y + 1
+  local speed = 0
+  local lap_difference = new_position['lap'] - old_position['lap']
 
-  local lap_percentage = last_lap_percentage
+  if lap_difference >= 0 then
+    speed = lap_difference * track_info['max_steps'] - old_position['step'] + new_position['step']
+  else
+    speed = -((-lap_difference) * track_info['max_steps'] - new_position['step'] + old_position['step'])
+  end
+  speed = speed / max_velocity
+
+  return speed
+end
+
+function MarioKartState:_get_time_from_ram()
+  return memory.read_u16_le(0x5C80)
+end
+
+function MarioKartState:_get_position_from_ram()
+	local old_domain = memory.getcurrentmemorydomain()
+
+	memory.usememorydomain("OAM")
+
+  local x = bit.band(memory.read_u16_le(2), 0x1FF) + 4
+	local y = memory.read_u8(0) + 4
+
+	memory.usememorydomain(old_domain)
+
+	return {x - minimap_offset_x + 1, y - minimap_offset_y + 1}
+end
+
+function MarioKartState:update_from_ram(track_info)
+  self.position = self:_get_position_from_ram()
+
+  local x = self.position[1]
+  local y = self.position[2]
+  local lap_percentage = self.last_lap_percentage
 
   if (track_info['matrix'][x][y] ~= -1) then
     lap_percentage = (track_info['matrix'][x][y] * 1.0) / track_info['max_steps']
   end
 
   -- Check if the lap counter is still valid
-  if last_lap_percentage >= end_of_lap_threshold and lap_percentage <= start_of_lap_threshold then
-    actual_global_lap = actual_global_lap + 1
-  else
-    if lap_percentage >= end_of_lap_threshold and last_lap_percentage <= start_of_lap_threshold then
-      actual_global_lap = actual_global_lap - 1
-    end
+  if self.last_lap_percentage >= end_of_lap_threshold and lap_percentage <= start_of_lap_threshold then
+    self.actual_global_lap = self.actual_global_lap + 1
+  elseif lap_percentage >= end_of_lap_threshold and self.last_lap_percentage <= start_of_lap_threshold then
+    self.actual_global_lap = self.actual_global_lap - 1
   end
 
-  last_lap_percentage = lap_percentage
-  global_lap = math.max(global_lap, actual_global_lap)
+  self.last_lap_percentage = lap_percentage
+  self.global_lap = math.max(self.global_lap, self.actual_global_lap)
 
-  if global_lap == actual_global_lap then
-    current_lap_percentage = lap_percentage
+  if self.global_lap == self.actual_global_lap then
+    self.current_lap_percentage = lap_percentage
   end
 
-  local new_position = {position=track_info['matrix'][x][y], lap=actual_global_lap}
-  push_position(new_position)
-  local old_position = get_oldest_position()
-  local speed = 0
+  self:_record_position({
+    step=track_info['matrix'][x][y],
+    lap=self.actual_global_lap,
+    x=x,
+    y=y
+  })
 
-  local lap_difference = new_position['lap'] - old_position['lap']
-
-  if lap_difference >= 0 then
-    speed = lap_difference * track_info['max_steps'] - old_position['position'] + new_position['position']
-  else
-    speed = -((-lap_difference) * track_info['max_steps'] - new_position['position'] + old_position['position'])
-  end
-  speed = speed / max_velocity
-
-   -- gui.text(0, 80, "speed: " .. speed .. '%')
-
-  return speed
+  self.time = self:_get_time_from_ram()
 end
 
-function get_minimap_position()
-	local old_domain = memory.getcurrentmemorydomain()
-	memory.usememorydomain("OAM")
-	local y = memory.read_u8(0) + 4
-	local x = bit.band(memory.read_u16_le(2), 0x1FF) + 4
-
-	memory.usememorydomain(old_domain)
-
-	return {x, y}
+function MarioKartState:get_reward(track_info)
+  return self:_get_speed(track_info)
 end
 
-function race_ended()
-  --return memory.read_u8(0x3BE0) == 0x34
-  return current_lap_percentage >= 0.15
+function MarioKartState:race_ended()
+  return self.current_lap_percentage >= lap_checkpoint_interval
 end
 
 function make_json_request(url, method, content_table, response_out)
@@ -133,8 +145,6 @@ function make_json_request(url, method, content_table, response_out)
     sink = ltn12.sink.table(response_out)
   }
 end
-
-local base_url = "http://localhost:5000/"
 
 function renew_game_id()
   local result = {}
@@ -156,37 +166,25 @@ function retrieve_minimap(name)
   return result
 end
 
-local game_id = nil
-local screenshot_folder = "../results/"
-local state_file = "../game/mario_kart.State"
-
-console.log(game_id)
-
-memory.usememorydomain("IWRAM")
-
-local track_info = retrieve_minimap('peach_circuit')
-
 function reset()
+  memory.usememorydomain("IWRAM")
   frame_number = 0
   game_id = renew_game_id()
   savestate.load(state_file)
-  current_lap_percentage = 0
-  last_lap_percentage = 0.9
-  lap_reward = {0, 0, 0}
-  actual_global_lap = 0
-  global_lap = 1
-  last_updated_lap = -1
-  positions_queue = deque.new()
+  state = MarioKartState.new()
 end
 
 reset()
+local track_info = retrieve_minimap('peach_circuit')
 
 while true do
-  local reward = compute_reward(track_info)
+  state:update_from_ram(track_info)
+  local reward = state:get_reward(track_info)
+  local race_ended = state:race_ended()
 
   gui.text(0, 0, "Reward: " .. reward)
-  gui.text(0, 20, "Ended: " .. tostring(race_ended()))
-  gui.text(0, 40, "Lap: " .. (global_lap) .. ' / 3')
+  gui.text(0, 20, "Ended: " .. tostring(race_ended))
+  gui.text(0, 40, "Lap: " .. (state.global_lap) .. ' / 3')
 
   client.screenshot(screenshot_folder .. "screenshot" .. (frame_number % frames_to_stack) ..  ".png")
 
@@ -211,11 +209,11 @@ while true do
 
     local result = {}
     make_json_request(base_url .. "request-action", "POST", {
-        game_id=game_id,
-        reward=reward,
-        screenshots=last_screenshots,
-        train=train,
-        race_ended=(race_ended() or out_of_time)
+      game_id=game_id,
+      reward=reward,
+      screenshots=last_screenshots,
+      train=train,
+      race_ended=(race_ended or out_of_time)
     }, result)
 
     result = json:decode(result[1])
@@ -224,11 +222,9 @@ while true do
 
   joypad.set(action)
 
-  if out_of_time or race_ended() then
+  if out_of_time or race_ended then
     reset()
   end
-
-	local position = get_minimap_position()
 
   frame_number = frame_number + 1
   emu.frameadvance()
